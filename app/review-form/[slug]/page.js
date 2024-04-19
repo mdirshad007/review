e054a1011be09ed3b21cf8ec18261557540d1555
Md Irshad <112070222+mdirshad007@users.noreply.github.com>
2024-04-19 14:59:11 +0530

    sefrwetew

--- app/review-form/[slug]/page.js
+++ app/review-form/[slug]/page.js
@@ -1,12 +1,14 @@
 "use client";
+import React, { useEffect } from 'react';
 import Button from "@/app/components/common/Button/Button";
 import InputFields from "@/app/components/common/FormElement/InputFields";
 import Radio from "@/app/components/common/FormElement/Radio";
 import StarRating from "@/app/components/common/FormElement/StarRating";
 import Textarea from "@/app/components/common/FormElement/Textarea";
 
-export default function page({params}) {
-  const radioData=["Yes","No"]
+export default function Page({ params }) { // Changed function name to start with an uppercase letter
+  const radioData=["Yes","No"];
+  
   useEffect(() => {
     async function fetchData() {
       try {
@@ -20,24 +22,24 @@ export default function page({params}) {
   
     fetchData();
   }, []);
+
   return (
     <section>
-     
       <div className="max-w-[1140px] mx-auto py-10 px-5">
-      <p>slug {params.slug}</p>
+        <p>slug {params.slug}</p>
         <form method="post" action="">
-            <InputFields labelName="Name" placeholder="Enter Your Name" id="name" />
-            <div className="flex gap-4 md:flex-row flex-col flex-wrap">
+          <InputFields labelName="Name" placeholder="Enter Your Name" id="name" />
+          <div className="flex gap-4 md:flex-row flex-col flex-wrap">
             <StarRating labelName="Did you find the restaurant's atmosphere and cleanliness satisfactory? " className="mt-3 mb-3 text-base" mainClassName="w-[49%]" />
             <StarRating labelName="How quickly and efficiently was your service provided?" className="mt-3 mb-3 text-base" mainClassName="w-[49%]"/>
             <StarRating labelName="Was the menu diverse enough to match your tastes and preferences?" className="mt-3 mb-3 text-base" mainClassName="w-[49%]"/>
             <StarRating labelName="How did you like the taste and quality of the food you ordered?" className="mt-3 mb-3 text-base" mainClassName="w-[49%]"/>
-            </div>
-            <Radio mainClassName="mt-6 mb-3" labelName="Do you think our prices are fair considering the food and service quality?" radioData={radioData}/>
-            <Textarea labelName="Can you tell us about any specific dish or part of your experience that was particularly good or bad?"/>
-            <Button>Send</Button>
+          </div>
+          <Radio mainClassName="mt-6 mb-3" labelName="Do you think our prices are fair considering the food and service quality?" radioData={radioData}/>
+          <Textarea labelName="Can you tell us about any specific dish or part of your experience that was particularly good or bad?"/>
+          <Button>Send</Button>
         </form>
-        </div>
+      </div>
     </section>
-  )
+  );
 }
