1def30a7b953805ac29ea4093b83ec1cd8377b29
Md Irshad <112070222+mdirshad007@users.noreply.github.com>
2024-05-13 17:25:57 +0530

    add vercel file and add headers

--- app/add-store/ProfileForm/ProfileForm.js
+++ app/add-store/ProfileForm/ProfileForm.js
@@ -66,7 +66,11 @@ export default function ProfileForm() {
         const response = await fetch("https://review-reflection.vercel.app/store/profile", {
             method: "POST",
             headers: {
-                "Content-Type": "application/json"
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Credentials":true,
+                "Access-Control-Allow-Origin":"*",
+                "Access-Control-Allow-Methods":"GET,DELETE,PATCH,POST,PUT",
+                "Access-Control-Allow-Headers":"X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
             },
             body: JSON.stringify(storeData)
         });
