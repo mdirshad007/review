1cfacae263f8ca92b6fde5e70e9b32e1009588f3
irshad-sme <irshad@searchmyexpert.com>
2024-05-27 16:55:13 +0530

    add page not found redirect

--- app/review-form/[slug]/page.js
+++ app/review-form/[slug]/page.js
@@ -1,10 +1,11 @@
-"use client"
+"use client";
 import { useEffect, useState } from "react";
 import Button from "@/app/components/common/Button/Button";
 import Radio from "@/app/components/common/FormElement/Radio";
 import StarRating from "@/app/components/common/FormElement/StarRating";
 import Textarea from "@/app/components/common/FormElement/Textarea";
 import InputText from "@/app/components/common/FormElement/InputText";
+import { redirect } from "next/navigation";
 
 export default function Page({ params }) {
   const baseUrl = "https://rev-ref.s3.amazonaws.com/";
@@ -18,7 +19,9 @@ export default function Page({ params }) {
   let radioData = ["Yes", "No"];
 
   async function getStoreData(path) {
-    let sData = await fetch(`https://review-reflection.vercel.app/store/feedback/${path}`);
+    let sData = await fetch(
+      `https://review-reflection.vercel.app/store/feedback/${path}`
+    );
     sData = await sData.json();
     return sData;
   }
@@ -36,59 +39,67 @@ export default function Page({ params }) {
   }, []);
 
   const handleRatingValue = (questionId, ratingData) => {
-    if(!questionId){
-      console.log(questionId," ",ratingData) 
+    if (!questionId) {
+      console.log(questionId, " ", ratingData);
     }
     setRatings((prevRatings) => ({ ...prevRatings, [questionId]: ratingData }));
   };
 
   const handleTextValue = (questionId, textData) => {
-    if(!questionId){
-      console.log(questionId," ",textData) 
+    if (!questionId) {
+      console.log(questionId, " ", textData);
     }
-    setTextInputs((prevTextInputs) => ({ ...prevTextInputs, [questionId]: textData }));
+    setTextInputs((prevTextInputs) => ({
+      ...prevTextInputs,
+      [questionId]: textData,
+    }));
   };
 
   const handleRadioValue = (questionId, radioData) => {
-    if(!questionId){
-      console.log(questionId," ",radioData) 
+    if (!questionId) {
+      console.log(questionId, " ", radioData);
     }
-    setRadioSelections((prevRadioSelections) => ({ ...prevRadioSelections, [questionId]: radioData }));
+    setRadioSelections((prevRadioSelections) => ({
+      ...prevRadioSelections,
+      [questionId]: radioData,
+    }));
   };
 
   const handleSendFeedback = () => {
     // Combine all the data
-  const feedbackData = {
-    user_id: 4, // or whichever user ID you want to include
-    answer: {
-      ...ratings,
-      ...radioSelections,
-      ...textInputs
-    }
-  };
- 
-    // Send the feedback data to your API or perform further actions   
+    const feedbackData = {
+      user_id: 4, // or whichever user ID you want to include
+      answer: {
+        ...ratings,
+        ...radioSelections,
+        ...textInputs,
+      },
+    };
+
+    // Send the feedback data to your API or perform further actions
     console.log(feedbackData);
-    postFeedback(feedbackData)
-    return feedbackData
+    postFeedback(feedbackData);
+    return feedbackData;
   };
 
-  const  postFeedback=async (feedbackData)=>{
-    let response=await fetch("https://review-reflection.vercel.app/store/feedback",{
-        method:"POST",
+  const postFeedback = async (feedbackData) => {
+    let response = await fetch(
+      "https://review-reflection.vercel.app/store/feedback",
+      {
+        method: "POST",
         headers: {
           "Content-Type": "application/json",
           // "Access-Control-Allow-Credentials":true,
           // "Access-Control-Allow-Origin":"https://review-kappa-two.vercel.app",
           // "Access-Control-Allow-Methods":"GET,DELETE,PATCH,POST,PUT",
           // "Access-Control-Allow-Headers":"X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-      },
-        body:JSON.stringify(feedbackData)
-    }
+        },
+        body: JSON.stringify(feedbackData),
+      }
     );
-    response= await response.json()
+    response = await response.json();
     console.log(response);
-}
+  };
 
   // useEffect(() => {
   //   const postData = handleSendFeedback();
@@ -98,8 +109,10 @@ export default function Page({ params }) {
     e.preventDefault();
     console.log("refresh prevented");
   };
-  
-  return (
+
+  return storeData?.statusCode == "404" ? (
+    redirect("/not-found")
+  ) : (
     <section>
       <div className="max-w-[1140px] mx-auto py-10 px-5">
         {storeData && (
@@ -111,46 +124,59 @@ export default function Page({ params }) {
               src={baseUrl + storeData.bg_img}
               alt={storeData.store_name}
             />
-            <h1 className="text-4xl mb-3 capitalize">Welcome to {storeData.store_name}</h1>
+            <h1 className="text-4xl mb-3 capitalize">
+              Welcome to {storeData.store_name}
+            </h1>
             <p className="mb-5">{storeData.tag_line}</p>
             <form onSubmit={onSubmit}>
-            {storeData?.questions?.map((item, id) => {
-              switch (item.answer_type) {
-                case "single integer":
-                  return (
-                    <StarRating
-                      labelName={item?.question_text}
-                      key={id}
-                      className="mt-3 mb-3 text-base"
-                      mainClassName="w-[49%] relative pb-[25px]"
-                      ratingCallBack={(ratingData) => handleRatingValue(item.question_store_id, ratingData)}
-                    />
-                  );
-                case "Boolean":
-                  return (
-                    <Radio
-                      mainClassName="mt-6 mb-3"
-                      labelName={item.question_text}
-                      key={id}
-                      radioData={radioData}
-                      radioCallBack={(radioData) => handleRadioValue(item.question_store_id, radioData)}
-                    />
-                  );
-                case "string":
-                  return (
-                    <InputText
-                      labelName={item.question_text}
-                      key={id}
-                      sendDataToParent={(textData) => handleTextValue(item.question_store_id, textData)}
-                    />
-                  );
-                case "paragraph":
-                  return <Textarea labelName={item.question_text} key={item.question_store_id} />;
-                default:
-                  return null;
-              }
-            })}
-            <Button onClick={handleSendFeedback}>Send</Button>
+              {storeData?.questions?.map((item, id) => {
+                switch (item.answer_type) {
+                  case "single integer":
+                    return (
+                      <StarRating
+                        labelName={item?.question_text}
+                        key={id}
+                        className="mt-3 mb-3 text-base"
+                        mainClassName="w-[49%] relative pb-[25px]"
+                        ratingCallBack={(ratingData) =>
+                          handleRatingValue(item.question_store_id, ratingData)
+                        }
+                      />
+                    );
+                  case "Boolean":
+                    return (
+                      <Radio
+                        mainClassName="mt-6 mb-3"
+                        labelName={item.question_text}
+                        key={id}
+                        radioData={radioData}
+                        radioCallBack={(radioData) =>
+                          handleRadioValue(item.question_store_id, radioData)
+                        }
+                      />
+                    );
+                  case "string":
+                    return (
+                      <InputText
+                        labelName={item.question_text}
+                        key={id}
+                        sendDataToParent={(textData) =>
+                          handleTextValue(item.question_store_id, textData)
+                        }
+                      />
+                    );
+                  case "paragraph":
+                    return (
+                      <Textarea
+                        labelName={item.question_text}
+                        key={item.question_store_id}
+                      />
+                    );
+                  default:
+                    return null;
+                }
+              })}
+              <Button onClick={handleSendFeedback}>Send</Button>
             </form>
           </>
         )}
