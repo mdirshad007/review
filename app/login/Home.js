e6f6c3125737260b48499a9a69a8eeb940b6fa0b
irshad-sme <irshad@searchmyexpert.com>
2024-05-28 11:42:55 +0530

    push login live

--- app/login/Home.js
+++ app/login/Home.js
@@ -8,7 +8,7 @@ export default function Home({ userDetails }) {
       <button
         onClick={() => {
           localStorage.clear();
-          window.location.reload();
+          // window.location.reload();
         }}
       >
         Logout
